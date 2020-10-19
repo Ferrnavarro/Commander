5c2af70f4dbc0c700241ac700dc984e27f1f4e2b
Fernando Magaña <fer.maganavarro@gmail.com>
2020-10-19 10:12:19 -0600

    Configure EntityFramework

--- Commander.API/Controllers/CommandsController.cs
+++ Commander.API/Controllers/CommandsController.cs
@@ -10,11 +10,11 @@ namespace Commander.API.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        private readonly ICommanderRepo _repository;
 
-        public CommandsController()
+        public CommandsController(ICommanderRepo repository)
         {
-            
+            _repository = repository;
         }
 
         [HttpGet]
